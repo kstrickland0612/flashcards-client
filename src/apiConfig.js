0cf84621f83c63ee2d5640070985365edbd048bc
kstrickland0612 <kstrickland0612@gmail.com>
2019-06-03 10:35:46 -0400

    Update apiConfig production url to deployed heroku

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://guarded-falls-36337.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
